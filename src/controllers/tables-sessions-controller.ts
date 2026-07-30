f5fbd211c39e8272f0aafb60a495d9cdc29539e1
theusan777 <theusant777@icloud.com>
2026-07-30 12:12:44 -0300

    feat: implement table sessions creation with validation and repository type definition

--- src/controllers/tables-sessions-controller.ts
+++ src/controllers/tables-sessions-controller.ts
@@ -1,10 +1,22 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction, request } from "express";
+import { knex } from "@/database/knex"
+import { z } from "zod"
 
 
 class TablesSessionsController {
   async create(req: Request, res: Response, next: NextFunction) {
     try {
-      
+      const bodySchema = z.object({
+        table_id: z.number(),
+      })
+
+      const { table_id } = bodySchema.parse(req.body)
+
+      await knex<TableSessionsRepository>("tables_sessions").insert({
+        table_id,
+        opened_at: knex.fn.now(),
+      })
+
       return res.status(201).json({ message: "Table session created successfully" });
     } catch (error) {
       next(error);
